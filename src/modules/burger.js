3128ff7ca5c5e20024a5074bcc09740cfec3cbb7
Julia Grib <yulgrib@gmail.com>
2023-03-29 10:45:14 +0300

    fix: change burger

--- src/modules/burger.js
+++ src/modules/burger.js
@@ -1,6 +1,11 @@
 const burgerIcon = document.querySelector('.burger-icon');
 const burger = document.querySelector('.burger');
+const burgerElem = document.querySelectorAll('.burger__list-elem');
 
-burgerIcon.addEventListener('click', () => {
+burgerIcon.addEventListener('click', showBurger);
+burgerElem.forEach((elem) => elem.addEventListener('click', showBurger));
+
+function showBurger() {
   burger.classList.toggle('burger_active');
-});
+  document.body.classList.toggle('overflow');
+}
